fbbb52f0ca9b77bf9ae7b6682b34a14a2262c217
Mark Birch <mark.birch@gmail.com>
2019-09-24 17:28:58 +0100

    updating dev endpoints

--- modules/ozoneBidAdapter.js
+++ modules/ozoneBidAdapter.js
@@ -10,15 +10,18 @@ const BIDDER_CODE = 'ozone';
 // const OZONEURI = 'http://pbs.pootl.net/openrtb2/auction';
 // const OZONECOOKIESYNC = 'http://pbs.pootl.net/static/load-cookie.html';
 
-const OZONEURI = 'http://afsheen-dev.the-ozone-project.com/openrtb2/auction';
-const OZONECOOKIESYNC = 'http://afsheen-dev.the-ozone-project.com/static/load-cookie.html';
+//dev
+const OZONEURI = 'http://test.ozpr.net/openrtb2/auction';
+const OZONECOOKIESYNC = 'http://test.ozpr.net/static/load-cookie.html';
+const OZONE_RENDERER_URL = 'https://prebid.the-ozone-project.com/ozone-renderer.js';
 
+//prod
 // const OZONEURI = 'https://elb.the-ozone-project.com/openrtb2/auction';
 // const OZONECOOKIESYNC = 'https://elb.the-ozone-project.com/static/load-cookie.html';
+// const OZONE_RENDERER_URL = 'http://silvermine.io/ozone/publishers/telegraph/ozone_files/ozone-renderer-jw-unruly.js';
+
 // const OZONECOOKIESYNC = 'http://local.bussongs.com/prebid-cookie-sync-development.html';
 
-const OZONE_RENDERER_URL = 'https://prebid.the-ozone-project.com/ozone-renderer.js';
-// const OZONE_RENDERER_URL = 'http://silvermine.io/ozone/publishers/telegraph/ozone_files/ozone-renderer-jw-unruly.js';
 
 const OZONEVERSION = '2.1.3';
 
